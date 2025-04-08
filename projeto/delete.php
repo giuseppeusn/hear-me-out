5fc67f935a4c30037fbf044e35c3259a58310aa6
Joao Cintra <joao.gabrielccosta9@gmail.com>
2025-04-08 20:13:33 -0300

    arquivos movidos para dentro da pasta usuario atualizado

--- projeto/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['id'])) {
-    $oMysql = connect_db();
-    $query = "DELETE FROM usuario WHERE id = ".$_GET['id'];
-    $oMysql->query($query);
-    header('location: index.php');
-    
-}
-?>
