3f07288169e65512160ce0245541e55ada99566f
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2025-04-08 19:53:21 -0300

    Adicionado header e index

---
+++ projeto/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CRUD</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-light">
+  <div class="container-fluid justify-content-between">
+    <ul class="navbar-nav">
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="/hear-me-out/projeto">Home</a>
+      </li> -->
+      <li class="nav-item">
+        <a class="nav-link" href="/hear-me-out/projeto/artista/">Artista</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/hear-me-out/projeto/album/">Álbum</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Críticos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Músicas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Usuários</a>
+      </li>
+      
+    </ul>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Cadastro
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Artista</a></li>
+            <li><a class="dropdown-item" href="#">Crítico</a></li>
+            <li><a class="dropdown-item" href="#">Usuário</a></li>
+          </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</body>
+</html>
---
+++ projeto/index.php
@@ -0,0 +1,5 @@
+<?php
+  include("header.php");
+  include("connect.php");
+  include("album/album.php")
+?>
