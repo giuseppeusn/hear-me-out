8d25579568be7cbc5120266c8188c78c4a634464
Giuseppe Nunes <giuseppeuhlmann@gmail.com>
2025-04-08 21:10:50 -0300

    Ajustado links redirecionamento

--- projeto/header.php
+++ projeto/header.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>CRUD</title>
+  <title>Hear me out</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-expand-sm bg-light">
   <div class="container-fluid justify-content-between">
     <ul class="navbar-nav">
@@ -25,12 +24,11 @@
         <a class="nav-link" href="/hear-me-out/projeto/critico/">Críticos</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Músicas</a>
+        <a class="nav-link" href="/hear-me-out/projeto/musica/">Músicas</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Usuários</a>
+        <a class="nav-link" href="/hear-me-out/projeto/critico/">Usuários</a>
       </li>
-      
     </ul>
     <ul class="navbar-nav">
       <li class="nav-item">
@@ -38,17 +36,16 @@
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Cadastro
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
-            <li><a class="dropdown-item" href="/hear-me-out/projeto/artista?page=1">Artista</a></li>
-            <li><a class="dropdown-item" href="/hear-me-out/projeto/critico?page=1">Crítico</a></li>
-            <li><a class="dropdown-item" href="/hear-me-out/projeto/usuario?page=1">Usuário</a></li>
-          </ul>
+          Cadastro
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
+          <li><a class="dropdown-item" href="/hear-me-out/projeto/artista?page=1">Artista</a></li>
+          <li><a class="dropdown-item" href="/hear-me-out/projeto/critico?page=1">Crítico</a></li>
+          <li><a class="dropdown-item" href="/hear-me-out/projeto/usuario?page=1">Usuário</a></li>
+        </ul>
       </li>
     </ul>
   </div>
 </nav>
-
 </body>
 </html>
