11423fced1650e241949bbd294a2e4cb34431241
dogo-o <gabriel.luanh@yahoo.com.br>
2025-04-21 15:14:43 -0300

    fixed paths 2.0

---
+++ projeto/connect.php
@@ -0,0 +1,12 @@
+<?php
+  function connect_db() {
+    $db_name = "hear_me_out";
+    $db_user = "root";
+    $db_pass = "";
+    $db_host = "localhost:3307";
+    
+    $connection = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    return $connection;
+  }
+?>
