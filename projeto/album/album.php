ac2c80848bff0600548cf27528585b0544302a20
Schnitzel-Semmel <phenrique.ida@gmail.com>
2025-05-14 23:40:54 -0300

    Listagem de album alterada de tabela -> card;

--- projeto/album/album.php
+++ projeto/album/album.php
@@ -1,63 +1,59 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
 <body>
+
 <div class="container mt-3">
-    <h2>Lista de álbuns</h2>
-    <a type="button" class="btn btn-success" href="index.php?page=1">Inserir novo álbum</a>
-    <table id=tabelaAlbum class="table table-striped">
-        <thead>
-            <tr>
-                <th>Capa</th>
-                <th>Nome</th>
-                <th>Artista</th>
-                <th>Data de lançamento</th>
-                <th>Número de músicas</th>
-                <th>Duração</th>
-                <th>#</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php
-        include_once("../connect.php");
+  <h2>Lista de álbuns</h2>
+  <a type="button" class="btn btn-success mb-3" href="index.php?page=1">Inserir novo álbum</a>
 
-        $conexao = connect_db(); 
-        $query = "
-        SELECT 
-          album.id AS album_id,
-          album.nome AS album_nome,
-          album.duracao,
-          album.capa,
-          album.data_lancamento,
-          album.qtd_musicas,
-          artista.nome AS artista_nome
-        FROM album
-        JOIN artista ON album.id_artista = artista.id
-      ";
-        $resultado = $conexao->query($query);
-        
-        if ($resultado) {
-            while ($linha = $resultado->fetch_object()) {
-                $btn = "<a href='index.php?page=2&id=" . $linha->album_id . "' class='btn btn-warning'>Alterar</a>";
-                $btn .= "<a href='index.php?page=3&id=" . $linha->album_id . "' class='btn btn-danger'>Excluir</a>";
+  <div class="row">
+    <?php
+    include_once("../connect.php");
+    $conexao = connect_db(); 
 
-                $duracao = $linha->duracao;  
-                $minutos = floor($duracao / 60);  
-                $segundos = $duracao % 60; 
+    $query = "
+      SELECT 
+        album.id AS album_id,
+        album.nome AS album_nome,
+        album.duracao,
+        album.capa,
+        album.data_lancamento,
+        album.qtd_musicas,
+        artista.nome AS artista_nome
+      FROM album
+      JOIN artista ON album.id_artista = artista.id
+    ";
 
-                echo "<tr>";
-                echo "<td><img src='{$linha->capa}' width='100'></td>";
-                echo "<td>{$linha->album_nome}</td>";
-                echo "<td>{$linha->artista_nome}</td>";
-                echo "<td>{$linha->data_lancamento}</td>";
-                echo "<td>{$linha->qtd_musicas}</td>";
-                echo "<td>{$minutos} min {$segundos} sec</td>";
-                echo "<td>" . $btn . "</td>";                
-                echo "</tr>";
-            }
+    $resultado = $conexao->query($query);
+
+    if ($resultado) {
+        while ($linha = $resultado->fetch_object()) {
+            $btnAlterar = "<a href='index.php?page=2&id=" . $linha->album_id . "' class='btn btn-warning me-2'>Alterar</a>";
+            $btnExcluir = "<a href='index.php?page=3&id=" . $linha->album_id . "' class='btn btn-danger'>Excluir</a>";
+
+            echo "
+            <div class='col-md-4 mb-4'>
+              <div class='card' style='width:100%'>
+                <img class='card-img-top' src='{$linha->capa}' alt='Capa do álbum'>
+                <div class='card-body'>
+                  <h4 class='card-title'>{$linha->album_nome}</h4>
+                  <p class='card-text'><strong>Qtd de músicas:</strong> {$linha->qtd_musicas}</p>
+                  <p class='card-text'><strong>Artista:</strong> {$linha->artista_nome}</p>
+                  <a href='#' class='btn btn-primary me-2'>Ver álbum</a>
+                  $btnAlterar
+                  $btnExcluir
+                </div>
+              </div>
+            </div>
+            ";
         }
-        ?>
-        </tbody>
-    </table>
+    }
+    ?>
+  </div>
 </div>
+
 </body>
 </html>
