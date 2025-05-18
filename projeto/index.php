b20016023ae008284cece925ddd5f98ad11df5d2
giuseppeusn <giuseppeuhlmann@gmail.com>
2025-05-17 23:08:22 -0300

    Corrigida verificação de login na home

--- projeto/index.php
+++ projeto/index.php
@@ -2,17 +2,5 @@
   include("header.php");
   include("connect.php");
   include("footer.php");
-
-
-  if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-  }
-
-  if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
-    header("location: /hear-me-out/projeto/login.php");
-    exit();
-  }
-
-  
   include("home.php");
 ?>
